630421131633bc7ab007621b53c98417d031535b
Aniekwe Onyekachi Godwin <aniekweonyekachi@gmail.com>
2023-02-18 00:50:18 +0100

    Learning if/else if, C language

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,27 +3,26 @@
 #include <stdio.h>
 
 /**
- * main - Determine if a number is negative, positive or zero.
+ * main - Determine if a number is positive, negative or zero
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int n;
-    
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    if (n<0)
-    {
-        printf("%d is negative\n", n);
-    }
-    else if (n>0)
-    {
-        printf("%d is positive\n", n);
-    }
-    else
-    {
-        printf("%d is zero\n", n);
-    }
-    return(0);
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n<0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
 }
