a97f227986531d49847e319f67f71ce2856e1dfa
Aniekwe Onyekachi Godwin <aniekweonyekachi@gmail.com>
2023-02-21 22:59:37 +0100

    Printing alphabets ten times in lower case

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 	int num;
 	int alpha;
 
-	for (num = 0; num < 9; num++)
+	for (num = 0; num <= 10; num++)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 			_putchar(alpha);
