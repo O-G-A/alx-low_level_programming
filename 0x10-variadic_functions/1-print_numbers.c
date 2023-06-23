a55eeb2d54a5297d58bee2e5dd2936c540caf956
Aniekwe Onyekachi Godwin <aniekweonyekachi@gmail.com>
2023-06-23 21:07:13 +0100

    Updated

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,8 +5,8 @@
 /**
  * print_numbers - prints numbers, followed by new line.
  * @separator: string to be printed.
- * @n: number of integers passed to the function.
- * ..: a variable number of numbers to be printed.
+ * @n: number of integers passed to function.
+ * @...: a variable number of numbers to be printed.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
@@ -17,8 +17,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (Indx = 0; Indx < n; Indx++)
 	{
 		printf("%d", va_arg(Nmbs, int));
+
 		if (Indx != (n - 1) && separator != NULL)
-				printf("%s", separator);
+			printf("%s", separator);
 	}
 
 	printf("\n");
